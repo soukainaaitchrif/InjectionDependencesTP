6c53067851504e39e31edb6f7cadbe12bcbf5251
Soukac <soukainaaitchrif2004@gmail.com>
2025-02-24 15:45:25 +0000

    Injection en utilisant spring versionXML

--- src/main/java/org/example/presentation/PresentationV3.java
+++ src/main/java/org/example/presentation/PresentationV3.java
@@ -1,7 +1,16 @@
 package org.example.presentation;
 
+import org.example.metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class PresentationV3 {
     public static void main(String[] args) {
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        IMetier metier= (IMetier) context.getBean(IMetier.class);
+        System.out.println(metier.calcul());
+
 
 
     }
